d3eefef4605ad9a9d146a938fa496f019c090acd
Thiago Assi <thiago-assi@outlook.com>
2024-04-02 20:21:24 -0300

    feat: turn project into an azure function

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { loadEnvironmentVariables } from './util/load-environment-variables';
 // Load the environment variables and check for errors
 loadEnvironmentVariables();
 
-import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 import logger from './loggers/default-logger';
+
+import { app, HttpResponseInit } from '@azure/functions';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+import { LovelaceSubscribedGameUrl } from './models/lovelace-subscribed-game-url.model';
 import { getDatabaseSubscribedPages } from './notion-api/get-database-subscribed-pages';
 import { mapDatabaseSubscribedPagesToLovelaceSubscribedGamesUrl } from './notion-api/map-database-subscribed-pages';
-import { LovelaceSubscribedGameUrl } from './models/lovelace-subscribed-game-url.model';
 
-async function main() {
+export async function main(): Promise<HttpResponseInit> {
   try {
     // Start the process by getting the database pages from the Notion API
     const databaseSubscribedPages: QueryDatabaseResponse = await getDatabaseSubscribedPages(
@@ -20,14 +22,32 @@ async function main() {
     const subscribedGamesUrl: LovelaceSubscribedGameUrl[] =
       mapDatabaseSubscribedPagesToLovelaceSubscribedGamesUrl(databaseSubscribedPages);
 
-    // Abort if there are no games
-    if (subscribedGamesUrl.length === 0) return;
+    return {
+      status: 200,
+      body: JSON.stringify(subscribedGamesUrl),
+    };
   } catch (e) {
+    let errorMessage = 'Internal server error';
+
     if (e instanceof Error) {
-      logger.error(e.message);
-      return;
+      errorMessage = e.message;
     }
+
+    if (typeof e === 'string') {
+      errorMessage = e;
+    }
+
+    logger.error(errorMessage);
+
+    return {
+      status: 500,
+      body: JSON.stringify({ error: errorMessage }),
+    };
   }
 }
 
-main();
+app.http('LovelaceDataSync', {
+  methods: ['GET', 'POST'],
+  authLevel: 'function',
+  handler: main,
+});
